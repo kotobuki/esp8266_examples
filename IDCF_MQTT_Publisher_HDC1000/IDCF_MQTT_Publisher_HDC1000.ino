f4fccb69608ec3ea0359b5e7bafd54c26d54ba4b
Shigeru Kobayashi <kotobuki@gmail.com>
2015-11-24 21:41:40 +0900

    Modified to use 'standard' libraries

--- IDCF_MQTT_Publisher_HDC1000/IDCF_MQTT_Publisher_HDC1000.ino
+++ IDCF_MQTT_Publisher_HDC1000/IDCF_MQTT_Publisher_HDC1000.ino
@@ -1,81 +1,44 @@
-/*
- * References
- *
- * mqtt_auth by Ian Tester (originally by Nicholas O'Leary)
- * https://github.com/Imroy/pubsubclient/blob/master/examples/mqtt_auth/mqtt_auth.ino
- */
-
 #include <Wire.h>
 
 #include <ESP8266WiFi.h>
 
 // MQTTを扱うためのライブラリ
-// https://github.com/Imroy/pubsubclient
 #include <PubSubClient.h>
 
+// JSONを扱うためのライブラリ
+#include <ArduinoJson.h>
+
 // HDC1000を扱うためのライブラリ
-#include "HDC1000.h"
+#include <Adafruit_HDC1000.h>
 
 // 設定ファイル
 #include "config.h"
 
-WiFiClient wclient;
+WiFiClient wifiClient;
+PubSubClient client(wifiClient);
 
-// MQTTクライアント
-PubSubClient client(wclient, server);
-
-HDC1000 hdc1000;
+Adafruit_HDC1000 hdc1000 = Adafruit_HDC1000();
 
 // 温度と湿度を格納する変数
 float temperature = 0.0;
 float humidity = 0.0;
 
 // コールバック関数
-void callback(const MQTT::Publish& pub) {
+void callback(char* topic, byte* payload, unsigned int length) {
   // コールバックで必要な処理があればここで
-}
-
-// JSONフォーマットでペイロードを用意
-String getPayloadInJson() {
-  String json = "{";
-  json +=  "\"devices\":";
-  json += "[\"";
-  json += action_1_uuid;
-  json += "\"";
-  json += ",";
-  json += "\"";
-  json += action_2_uuid;
-  json += "\"";
-  json += "],";
-  json += "\"payload\":";
-  json += "{";
-  json += "\"temperature\":\"";
-  json += temperature;
-  json += "\",";
-  json += "\"humidity\":\"";
-  json += humidity;
-  json += "\"}";
-  json += "}";
-
-  return json;
-}
-
-// 浮動小数点数を小数点以下2桁でシリアルにプリント
-void printFormattedFloat(float val) {
-  char buffer[10];
-
-  dtostrf(val, 4, 2, buffer);
-  Serial.print(buffer);
 }
 
 void setup() {
   // I2Cの通信を開始
   // SDA: DIO4
-  // SCL: DIO14
-  Wire.begin(4, 14);
+  // SCL: DIO5
+  Wire.begin();
 
   // シリアル通信を開始
   Serial.begin(115200);
+  delay(10);
+  Serial.println();
+  Serial.println();
 
   // HDC1000を設定
   hdc1000.begin();
@@ -87,53 +50,64 @@ void loop() {
     Serial.print("Connecting to ");
     Serial.print(ssid);
     Serial.println("...");
-    WiFi.begin(ssid, pass);
+    WiFi.begin(ssid, password);
 
     if (WiFi.waitForConnectResult() != WL_CONNECTED) {
-      Serial.println("Failed");
+      // Wi-Fiアクスポイントへの接続に失敗したら5秒間待ってリトライ
+      Serial.println("Failed to connect");
+      delay(5000);
       return;
     } else {
-      Serial.println("WiFi connected");
+      Serial.print("WiFi connected: ");
+      Serial.println(WiFi.localIP());
+      client.setServer(server, 1883);
     }
   }
 
-  // クライアントがICDF Cloudのサーバに接続されていなければ接続
+  // クライアントがICDF Cloudのサーバに接続されていなければ
   if (!client.connected()) {
-    // トリガー1のUUIDとトークンで認証
-    MQTT::Connect mqttConnect(mqtt_client_id);
-    mqttConnect.set_auth(trigger_1_uuid, trigger_1_token);
+    // トリガー1のUUIDとトークンをユーザ名およびパスワードとしてサーバに接続
+    client.connect(mqtt_client_id, trigger_1_uuid, trigger_1_token);
 
-    if (client.connect(mqttConnect)) {
-      client.set_callback(callback);
-      Serial.println("Client connected");
+    if (client.connected()) {
+      Serial.print("MQTT connected: ");
+      Serial.println(server);
+      client.setCallback(callback);
     } else {
-      Serial.println("Client failed to connect");
+      Serial.print("MQTT connection failed: ");
+      Serial.println(client.state());
+      delay(5000);
     }
-  }
+  } else {
+    // 既にサーバに接続されていれば通常処理を行う
+    client.loop();
 
-  // クライアントがICDF Cloudのサーバに接続されていたら以下の処理を実行
-  if (client.connected()) {
-    // HDC1000で1度だけ測定を行う
-    hdc1000.readMeasurements();
-
+    // クライアントがICDF Cloudのサーバに接続されていたら以下の処理を実行
     // 温度と湿度を取得
-    temperature = hdc1000.getTemperature();
-    humidity = hdc1000.getHumidity();
+    temperature = hdc1000.readTemperature();
+    humidity = hdc1000.readHumidity();
 
-    // 温度と湿度、気圧をシリアルにプリント
-    Serial.print("Temperature: ");
-    printFormattedFloat(temperature);
-    Serial.println("ºC");
-    Serial.print("Humidity   : ");
-    printFormattedFloat(humidity);
-    Serial.println("%");
+    // JSONフォーマットでペイロードを用意
+    StaticJsonBuffer<200> jsonBuffer;
+    JsonObject& root = jsonBuffer.createObject();
+    JsonArray& devices = root.createNestedArray("devices");
+    devices.add(action_1_uuid);
+//    devices.add(action_2_uuid);
+    JsonObject& payload = root.createNestedObject("payload");
+    payload["temperature"] = temperature;
+    payload["humidity"] = humidity;
+    root.prettyPrintTo(Serial);
+    Serial.println();
+
+    char buffer[256];
+    root.printTo(buffer, sizeof(buffer));
 
     // IDCF CloudにMQTTでデータを送信
-    bool sent = client.publish("message", getPayloadInJson());
+    bool sent = client.publish("message", buffer);
     if (sent) {
-      Serial.println("Sent");
+      Serial.println("Published");
     } else {
-      Serial.println("Failed to send");
+      Serial.println("Failed to publish");
     }
 
     Serial.println();
